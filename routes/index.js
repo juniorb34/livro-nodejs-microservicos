b87e5122c4bd6a3ed9095aa8a0e7b8420412c65b
juniorb34 <juniorb34fortal@gmail.com>
2020-11-03 13:29:16 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/new', function(req, res, next){
+  res.render('new', {title: "Cadastro de Cliente", action:"/new"})
+});
+
+router.post('/new', function(req, res, next){
+  res.redirect('/?new=true')
+})
+
+module.exports = router;
